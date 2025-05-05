500e2392405311d5aba4338eb3bad070f6f5fa92
hanzel_sc <hanselkansam04@gmail.com>
2025-05-05 15:14:25 +0530

    Incorporated Infrastructure Pentest tools - Tfsec and Terrascan

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Running basic Terrascan scan
-terrascan scan -t aws -d infrastructure/ -o json > output/terrascan_report.json
+terrascan scan -t aws -d infrastructure/ -o json > /home/zwi/terrascan_report.json
 
 # Running basic Tfsec scan
-tfsec infrastructure/ --format json > output/tfsec_report.json
+tfsec infrastructure/ --format json > /home/zwi/tfsec_report.json
